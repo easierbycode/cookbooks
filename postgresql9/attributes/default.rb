059e0eb9170e709a5ebcdbc2111431ac8d0194b2
Daniel Johnson <navseeker7@gmail.com>
2013-03-28 22:40:29 -0700

    update scalarium references to work with opscode

--- postgresql9/attributes/default.rb
+++ postgresql9/attributes/default.rb
@@ -13,6 +13,6 @@ default[:postgresql9][:temp_buffers] = '50MB'
 default[:postgresql9][:shared_buffers] = '200MB'
 default[:postgresql9][:port] = '5432'
 default[:postgresql9][:max_connections] = 1000
-default[:postgresql9][:role] = 'scalarium'
+default[:postgresql9][:role] = 'opsworks'
 default[:postgresql9][:password] = Digest::MD5.hexdigest(Digest::SHA1.hexdigest("--#{rand(10000)}--#{Time.now}--")[0,8])
 default[:postgresql9][:shmmax] = '1342177280'
